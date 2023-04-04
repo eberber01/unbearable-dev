a56bf8000dfc6cf8f4444cd8a2ead6fe9344c175
eric <eberber01@gmail.com>
2023-04-04 16:04:49 -0700

    Add Project URLS

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/ProjectItem.module.css";
 export default function ProjectItem(props: any) {
 	return (
 		<div className={styles.container}>
-			<a className={styles.title} href=''>
+			<a className={styles.title} href={props.href} target="_blank">
 				{props.title}
 			</a>
 			<p className={styles.text}>{props.children}</p>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,15 +2,18 @@ import styles from "../styles/Projects.module.css";
 import ProjectItem from "./ProjectItem";
 
 export default function Projects() {
+	const SNAKE_URL = "https://eberber01.github.io/snake/";
+	const QUILTIFY_URL = "https://eberber01.github.io/Quiltify/";
+
 	return (
 		<div className={styles.container} id="projects">
 			<h1>Projects</h1>
 			<div className={styles.grid}>
-				<ProjectItem title="Quiltify">
+				<ProjectItem title="Quiltify" href={QUILTIFY_URL}>
 					Spotify API Integrated application that displays users top
 					artists, built using AWS lambda functions
 				</ProjectItem>
-				<ProjectItem title="Snake">
+				<ProjectItem title="Snake" href={SNAKE_URL}>
 					Vanilla JavaScript game with HTML canvas
 				</ProjectItem>
 			</div>
