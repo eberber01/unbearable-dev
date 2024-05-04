17b7e8cad046a95885a95538b562b4b3dc32e0c1
eric <eberber01@gmail.com>
2024-05-04 12:13:16 -0700

    Update Projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,10 @@ import ProjectItem from "./ProjectItem";
 
 export default function Projects() {
 	const SNAKE_URL = "https://eberber01.github.io/snake/";
-	const QUILTIFY_URL = "https://main.d3ftg3vbkuts3x.amplifyapp.com/"
+	const QUILTIFY_URL = "https://main.d3ftg3vbkuts3x.amplifyapp.com/";
+	const AGORAPHOBIA_URL =
+		"https://users.csc.calpoly.edu/~eberbern/FinalProject.html";
+	const DINO_URL = "https://github.com/eberber01/image-generator"
 
 	return (
 		<div className={styles.container} id="projects">
@@ -14,7 +17,13 @@ export default function Projects() {
 					artists, built using AWS lambda functions
 				</ProjectItem>
 				<ProjectItem title="Snake" href={SNAKE_URL}>
-					Vanilla JavaScript game with HTML canvas
+					Vanilla JavaScript game using HTML canvas
+				</ProjectItem>
+				<ProjectItem title="Agoraphobia" href={AGORAPHOBIA_URL}>
+					Team developed short story game
+				</ProjectItem>
+				<ProjectItem title="Image Generator" href={DINO_URL}>
+					Python-based tool designed to create composite images by layering different components 
 				</ProjectItem>
 			</div>
 		</div>
